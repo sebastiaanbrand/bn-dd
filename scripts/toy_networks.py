d7caaf900785f471fec79a511992a539f819eff5
Sebastiaan Brand <sebastiaanbrand@gmail.com>
2022-11-03 16:15:36 +0100

    updated encoding of toy networks

--- scripts/toy_networks.py
+++ scripts/toy_networks.py
@@ -1,17 +1,18 @@
 from pgmpy.models import BayesianNetwork
 from pgmpy.factors.discrete import TabularCPD
+from pgmpy.readwrite import XMLBIFWriter
 
 def toy_network_1():
     model = BayesianNetwork([('A', 'B'), ('B', 'C')])
     cpd_a = TabularCPD('A', 2, [[0.2], [0.8]])
-    cpd_b = TabularCPD('B', 2, [[0.3, 0.7], [0.7, 0.3]],
+    cpd_b = TabularCPD('B', 2, [[0.3, 0.25], [0.7, 0.75]],
                         evidence=['A'],
                         evidence_card=[2])
-    cpd_c = TabularCPD('C', 2, [[0.1, 0.9], [0.9, 0.1]],
+    cpd_c = TabularCPD('C', 2, [[0.1, 0.8], [0.9, 0.2]],
                         evidence=['B'],
                         evidence_card=[2])
     model.add_cpds(cpd_a, cpd_b, cpd_c)
-    return model
+    return model, 'line'
 
 def toy_network_2():
     model = BayesianNetwork([('D', 'G'), ('I', 'G'), ('G', 'L'), ('I', 'S')])
@@ -31,4 +32,16 @@ def toy_network_2():
                                     [0.05, 0.8]],
                             evidence=['I'], evidence_card=[2])
     model.add_cpds(grade_cpd, diff_cpd, intel_cpd, letter_cpd, sat_cpd)
-    return model
+    return model, 'student'
+
+if __name__ == '__main__':
+
+        # write toy BN 1
+        model, name = toy_network_1()
+        writer = XMLBIFWriter(model)
+        writer.write_xmlbif(f"models/{name}.xmlbif")
+        
+        # write toy BN 2
+        model, name = toy_network_2()
+        writer = XMLBIFWriter(model)
+        writer.write_xmlbif(f"models/{name}.xmlbif")
