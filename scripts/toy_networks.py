eb074afafe4cd6ec683951ea328bb77145513130
Sebastiaan Brand <sebastiaanbrand@gmail.com>
2023-03-23 17:30:17 +0100

    added a toy network to use as example in the paper

--- scripts/toy_networks.py
+++ scripts/toy_networks.py
@@ -2,7 +2,11 @@ from pgmpy.models import BayesianNetwork
 from pgmpy.factors.discrete import TabularCPD
 from pgmpy.readwrite import XMLBIFWriter
 
+
 def toy_network_1():
+    """
+    Generate a simple line BN.
+    """
     model = BayesianNetwork([('A', 'B'), ('B', 'C')])
     cpd_a = TabularCPD('A', 2, [[0.2], [0.8]])
     cpd_b = TabularCPD('B', 2, [[0.3, 0.25], [0.7, 0.75]],
@@ -14,7 +18,11 @@ def toy_network_1():
     model.add_cpds(cpd_a, cpd_b, cpd_c)
     return model, 'line'
 
+
 def toy_network_2():
+    """
+    Generate a line BN with reoccuring values per CPT.
+    """
     model = BayesianNetwork([('A', 'B'), ('B', 'C')])
     cpd_a = TabularCPD('A', 2, [[0.5], [0.5]])
     cpd_b = TabularCPD('B', 2, [[0.3, 0.5], [0.7, 0.5]],
@@ -25,9 +33,12 @@ def toy_network_2():
                         evidence_card=[2])
     model.add_cpds(cpd_a, cpd_b, cpd_c)
     return model, 'line2'
-        
+
 
 def toy_network_3():
+    """
+    Generate the 'student' BN.
+    """
     model = BayesianNetwork([('D', 'G'), ('I', 'G'), ('G', 'L'), ('I', 'S')])
     grade_cpd  = TabularCPD(variable='G', variable_card=3,
                             values=[[0.3, 0.05, 0.9, 0.5],
@@ -47,7 +58,26 @@ def toy_network_3():
     model.add_cpds(grade_cpd, diff_cpd, intel_cpd, letter_cpd, sat_cpd)
     return model, 'student'
 
-if __name__ == '__main__':
+
+def paper_example():
+    """
+    Generate the 2 node example network from Fig ... in ... .
+    """
+    model = BayesianNetwork([('A','B')])
+    cpd_a = TabularCPD(variable='A', variable_card=2, values=[[0.2], [0.8]])
+    cpd_b = TabularCPD(variable='B', variable_card=3,
+                       values=[[0.5, 0.5],
+                               [0.3, 0.2],
+                               [0.2, 0.3]],
+                       evidence=['A'], evidence_card=[2])
+    model.add_cpds(cpd_a, cpd_b)
+    return model, 'paper_example'
+
+
+def main():
+    """'
+    Generate toy networks and write to XMLBIF.
+    """
 
     # write toy BN 1
     model, name = toy_network_1()
@@ -63,8 +93,17 @@ if __name__ == '__main__':
     model, _ = toy_network_2()
     writer = XMLBIFWriter(model)
     writer.write_xmlbif("models/line3.xmlbif")
-        
+
     # write toy BN 2
     model, name = toy_network_3()
     writer = XMLBIFWriter(model)
     writer.write_xmlbif(f"models/{name}.xmlbif")
+
+    # write paper example
+    model, name = paper_example()
+    writer = XMLBIFWriter(model)
+    writer.write_xmlbif(f"models/{name}.xmlbif")
+
+
+if __name__ == '__main__':
+    main()
