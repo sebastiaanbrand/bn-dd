afc2819401dd1432b5416d6ffd30157dd23cae0e
Sebastiaan Brand <sebastiaanbrand@gmail.com>
2023-05-31 12:33:46 +0200

    hardcode gcc/g++ paths as well

--- compile_sources_edd.sh
+++ compile_sources_edd.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# script to compile sources on EDD machine (manually configures GMP dirs)
+# script to compile sources on EDD machine (manually configures GMP and gcc/g++ paths)
 
 while getopts "r" opt; do
   case $opt in
@@ -16,7 +16,7 @@ cd dd_inference
 if [[ $recompile ]]; then rm -r -f sylvan_build; fi
 mkdir -p sylvan_build
 cd sylvan_build
-cmake -DGMP_INCLUDE_DIR=../../include -DGMP_LIBRARIES=/usr/lib64/libgmp.so.10.3.2 ../../extern/sylvan
+cmake -DGMP_INCLUDE_DIR=../../include -DGMP_LIBRARIES=/usr/lib64/libgmp.so.10.3.2 -DCMAKE_C_COMPILER=/opt/rh/gcc-toolset-10/root/usr/bin/gcc -DCMAKE_CXX_COMPILER=/opt/rh/gcc-toolset-10/root/usr/bin/g++ ../../extern/sylvan
 make
 cd ../..
 
@@ -25,7 +25,7 @@ cd dd_inference
 if [[ $recompile ]]; then rm -r -f build; fi
 mkdir -p build
 cd build
-cmake ..
+cmake -DCMAKE_C_COMPILER=/opt/rh/gcc-toolset-10/root/usr/bin/gcc -DCMAKE_CXX_COMPILER=/opt/rh/gcc-toolset-10/root/usr/bin/g++ ..
 make
 cd ../..
 
