fc4b916eb4cbb0ba21d84a0c3fd163678248a52e
Maarten Vonk <maartencvonk@gmail.com>
2024-02-07 15:18:35 +0100

    Update run.py
    update run.py with naive_do_operator and add additional unit tests

--- run.py
+++ run.py
@@ -15,20 +15,28 @@ S 19
 
 TODO: Objective function
 """
+import time
 import json
 import os
-from itertools import chain
+from itertools import chain,product
 from time import perf_counter
 from dataclasses import dataclass, field
 from typing import List, Set, Tuple
 from functools import cached_property
 import ioh
 import numpy as np
-
-
 import dd_inference as dd
 
-TEST_MODEL_PATH = "./models/tests/lg_EV30/data_lg_EV30"
+path = os.path.join(os.getcwd(), "models/do_test1MLE")
+TEST_MODEL_PATH = "./models/toy_networks/line"
+MODELS = (
+    "data_do_EV2",
+    "data_do_EV2"
+)
+
+MODEL_IDX = 1
+MODEL_PATH = path + '/' + MODELS[MODEL_IDX]
+
 
 @dataclass
 class Node:
@@ -53,8 +61,11 @@ class Node:
 
         with open(f"{model_name}settings.json") as f:
             data = json.load(f)
+            target_var, *_ = data["target_var"]
+            search_vars = data["search_vars"]
             for source, target in data["edges"]:
                 nodes[target].parents.append(nodes[source])
+            
 
         with open(f"{model_name}_value_dict.json") as f:
             data = json.load(f)
@@ -63,7 +74,8 @@ class Node:
                     map(lambda x: x[1], sorted(value.items(), key=lambda x: int(x[0])))
                 )
                 nodes[key].n_bins = len(nodes[key].bins)
-        return nodes
+                print(nodes, nodes[key].bins)
+        return nodes, target_var, search_vars
 
     def int_to_binary(self, value: int) -> Set[Tuple[int, bool]]:
         binary = np.binary_repr(value, self.n_idx)
@@ -71,12 +83,12 @@ class Node:
 
 
 class Objective:
-    def __init__(self, nodes: List[Node], diagram: dd.BnBdd, condition_node: Node):
-        self.nodes = [node for node in nodes if node != condition_node]
+    def __init__(self, nodes: List[Node], diagram: dd.BnBdd, condition_node: Node, search_nodes: List[str]):
+        self.nodes = [node for node in nodes if node.name in search_nodes]
         self.diagram = diagram
         self.dimension = len(self.nodes)
-        self.lb = np.zeros(len(self.nodes)) - 1
-        self.ub = np.array([node.n_bins for node in self.nodes])
+        self.lb = np.zeros(len(self.nodes), dtype=int) - 1
+        self.ub = np.array([node.n_bins - 1 for node in self.nodes], dtype=int)
         self.condition_node = condition_node
         self.max_y = max(self.condition_node.bins)
         self.condition_node_ops = [
@@ -111,72 +123,101 @@ class Objective:
         do_nodes = self.get_do_nodes(x)
         do_ops = self.get_do_ops(do_nodes)
         do_nodes = self.remap_do_nodes(do_ops, do_nodes)
-
         expectation = 0
         sum_prob = 0
-
         for condition, bin_value in self.condition_node_ops:
-            pr_marginal = dd.bnbdd_marginalize(self.diagram, condition.union(do_ops))
-            pr_total = 0
-            if pr_marginal == 0:
-                continue
-
             parents = set()
-            pr_do = 1.0
             for do_node, do_node_ops, do_ops_with_parent in do_nodes:
-                pdo = dd.bnbdd_condition(self.diagram, do_node_ops, do_ops_with_parent)
-                pr_do *= pdo
                 parents = parents.union(do_node.parent_ids)
-
-            pr_total = pr_marginal / pr_do
-            pr_total_new = dd.bnbdd_do_naive(self.diagram, do_ops, condition, parents)
-            print(pr_total, pr_total_new)
-            breakpoint()
-    
+            pr_total = dd.bnbdd_do_naive(self.diagram, condition, do_ops, parents)
             sum_prob += pr_total
             expectation += pr_total * bin_value
-        assert sum_prob == 0 or np.isclose(sum_prob, 1.0)
+        assert sum_prob == 0 or np.isclose(sum_prob, 1.0)  
         return expectation
 
     def __call__(self, x: List[int]):
         expectation = self.calc_expected_value(x)
         return self.max_y - expectation
 
+def set_objective(dim: int, iid: int):
+    return [float("nan")] * dim, 0.0
+
+
+def brute_force(problem, limit=1000):
+    obj = problem.bounds
+    possible_solutions = list(product(*map(lambda x: range(*x), zip(obj.lb, obj.ub + 1))))
+    n_possible_solutions = len(possible_solutions)
+    
+    if n_possible_solutions > limit:
+        print(n_possible_solutions, " is too much to brute force, quitting...")
+        return 
+    
+    print(f"Brute force evaluating {n_possible_solutions} solutions")
+    ymin = float("inf")
+    xmin = None
+    for x in possible_solutions:
+        y = problem(x)
+        if y < ymin:
+            ymin = y
+            xmin = x
+    print("best found: (x, y)", xmin, ymin)
+    print()
+    return xmin, ymin
 
 if __name__ == "__main__":
     tracepeak = True
     verbose = False
-    nodes = Node.read(TEST_MODEL_PATH)
+    budget = 5000
+    n_reps = 50
+    nodes, target_var, search_vars = Node.read(MODEL_PATH)
     np.random.seed(1)
     with dd.SylvanRunnable():
         print("loading model...", end=" ")
         start = perf_counter()
-        bnbdd = dd.bnbdd_from_files(TEST_MODEL_PATH, tracepeak, verbose)
+        bnbdd = dd.bnbdd_from_files(MODEL_PATH, tracepeak, verbose)
         print("time elapsed: ", perf_counter() - start, "s")
-
-        obj = Objective(list(nodes.values()), bnbdd, nodes["E"])
+        obj = Objective(list(nodes.values()), bnbdd, nodes[target_var], search_vars)
+        obj.calc_expected_value(x=[1,1])
+        sys.exit()
+        print(f'expectation{obj.calc_expected_value(x=[87])}')
+        print(f'expectation{obj.calc_expected_value(x=[1])}')
+        print(f'max_y{max(nodes[target_var].bins)}')
+        print(f'total{max(nodes[target_var].bins) - obj.calc_expected_value(x=[11, 7, 26])}')
         problem = ioh.wrap_problem(
             obj,
-            f"{os.path.basename(TEST_MODEL_PATH)}_objective",
+            f"{os.path.basename(MODEL_PATH)}_objective",
             ioh.ProblemClass.INTEGER,
             obj.dimension,
             0,
             ioh.OptimizationType.MIN,
             min(obj.lb),
             max(obj.ub),
+            calculate_objective=set_objective
         )
+        problem.bounds.ub = obj.ub
+        problem.enforce_bounds(float("inf"), how=ioh.ConstraintEnforcement.HARD)
 
-        print("Random search multiple ops")
-        x_best = None
-        f_best = np.inf
+        x, y = brute_force(problem)
+        
 
-        for _ in range(1000):
-            x0 = np.random.randint(-1, obj.ub, problem.meta_data.n_variables)
-            f0 = problem(x0)
 
-            print(x0, f0)
-            if f0 < f_best:
-                f_best = f0
-                x_best = x0
-        print(x_best, f_best)
-        print()
+
+        # logger = ioh.logger.Analyzer(
+        #     algorithm_name="intEA",
+        #     store_positions=True,
+        #     folder_name="intEA"
+        # )
+        # problem.attach_logger(logger)
+
+        # for run in range(n_reps):
+        #     es = UnboundedIntegerEA(mu=5, lambda_=20, budget=budget, verbose=True)
+        #     best = es(problem)
+        #     print(run, problem.state.evaluations, best)
+        #     problem.reset()
+
+
+        # for i in range(-1, 2):
+        #     for j in range(-1, 30):
+        #         x = [i, j]
+        #         print(x, problem(x))
+
